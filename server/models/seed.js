dace2bf2a5e0dccb6bb0b3af44f50d6b1cd177d7
Elnur Kurtaliev <el92@ya.ru>
2016-09-23 17:35:46 +0300

    Перенос функции в модель

--- server/models/seed.js
+++ server/models/seed.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = require('mongoose').Types.ObjectId;
+var re = {
+    nick: '@[a-z0-9_-]+',
+    hash: '#[^\\s]+',
+    link: 'https?:\\/\\/[^\\s]+'
+
+};
 
 var schema =  new Schema({
     msg: String,
@@ -119,7 +125,7 @@ schema.statics.getPlain = function (user, opts, callback) {
         var seedsPlain = seeds.map(function (seed) {
             return {
                 id: seed._id,
-                msg: seed.msg,
+                msg: wrapLinks(seed.msg),
                 datetime: seed.datetime,
                 parent: seed.parent, //Твит на который сделали ответ
                 profile: seed.user[0],
@@ -153,7 +159,7 @@ schema.statics.getSeed = function (seedId, callback) {
         var seed = seeds[0] || null;
         var result = {
                 id: seed._id,
-                msg: seed.msg,
+                msg: wrapLinks(seed.msg),
                 datetime: seed.datetime,
                 parent: seed.parent, //Твит на который сделали ответ
                 child: seed.child,
@@ -166,6 +172,39 @@ schema.statics.getSeed = function (seedId, callback) {
 };
 
 
+function wrapLinks(msg) {
+    var reNick = re.nick;
+    var reHash = re.hash;
+    var reLink = re.link;
+    msg = msg.split(new RegExp('(^|\\s)(' + reNick + '|' + reHash + '|' + reLink + ')', 'i'));
+    msg = msg.map(function (word) {
+        if (word.length) {
+            word = new RegExp('^' + reNick + '$', 'i').test(word) ? {
+                block: 'link',
+                url: '/profile/' + word.substr(1),
+                content: word
+            } : new RegExp('^' + reHash + '$').test(word) ? {
+                block: 'link',
+                url: '/search/?text=' + encodeURIComponent(word),
+                content: word
+            } : new RegExp('^' + reLink + '$', 'i').test(word) ? {
+                block: 'link',
+                url: word,
+                content: word,
+                target: '_blank'
+            } : word;
+        }
+        return word
+    });
+    return msg;
+};
+
+
+schema.virtual('msgWrapped').get(function () {
+    return wrapLinks(this.msg);
+});
+
+
 schema.post('save', function(doc) {
     if (doc.parent) {
         this.model('Seed').findByIdAndUpdate(doc.parent, {$push: {child: doc._id }}, function (err, doc){
@@ -176,14 +215,14 @@ schema.post('save', function(doc) {
 
 schema.pre('save', function(next) {
     if (this.msg) {
-        var tags = this.msg.match(/#.+?(\s|$)/g);
+        var tags = this.msg.match(new RegExp('(^|\\s)(' + re.hash + ')', 'g'));
         if (tags) {
             this.tags = tags.map(function(tag){
                 return tag.trim().substr(1);
             });
         }
 
-        var nicks = this.msg.match(/@[a-z0-9_-]+/ig);
+        var nicks = this.msg.match(new RegExp('(^|\\s)(' + re.nick + ')', 'ig'));
         if (nicks) {
             this.mention = nicks.map(function(nick){
                 return nick.trim().substr(1);
